784d4ca6b5f011b336e53db81de221065af7d820
Nam <ndbui03@gmail.com>
2021-10-24 22:26:49 -0400

    Merged music cog with music manager class
    Merged class Music with class MusicManager into single class Music. class Music now located in music.py

--- src/main.py
+++ src/main.py
@@ -6,7 +6,6 @@ from dotenv import load_dotenv
 import discord
 from discord.ext import tasks, commands
 from discord.utils import get
-from youtube_dl import YoutubeDL
 
 #load environment variables
 load_dotenv()
@@ -24,39 +23,6 @@ async def on_ready():
     print(f'{bot.user.name} has connected successfully!')
 
 #music commands
-class Music(commands.Cog):
-    '''music commands'''
-    def __init__(self, bot):
-        self.bot = bot
-        self.queue = []
-    
-    async def __join(self, ctx):
-        '''Joins voice channel if possible'''
-        channel = ctx.message.author.voice.channel
-        if channel == None:
-            await ctx.send("You must be in a voice channel to play music!")
-        else:
-            voice = get(self.bot.voice_clients, guild=ctx.guild)
-            if voice and voice.is_connected():
-                await voice.move_to(channel)
-            else:
-                voice = await channel.connect()
-
-    #play/add to queue
-    @commands.command(name = "play")
-    async def play(self, ctx, *source):
-        '''Give URL or search term | sources: -y = YT, -s = Spotify, -c = Soundcloud (default: YT)'''
-        #determine if URL or search term
-            #if URL, check if valid
-            #else, check source
-                #get URL
-        #add song to queue
-
-    #stop
-    @commands.command(name = "stop")
-    async def stop(self, ctx):
-        '''Stops playing music and leaves the call'''
-        pass
 
 bot.add_cog(Music(bot))
 
--- src/music.py
+++ src/music.py
@@ -1,5 +1,97 @@
 import os
+from discord.ext import tasks, commands
+from discord.utils import get
 from apiclient.discovery import build
+from youtube_dl import YoutubeDL
+
+class Music(commands.Cog):
+    '''music commands'''
+    def __init__(self, bot):
+        self.bot = bot
+        self.queue = []
+        #initialize sources and clients
+        self.__youtube = build('youtube','v3', developerKey = os.getenv("GOOGLE_TOKEN"))
+        self.__sources = {"-y": 0, "-s": 1, "-c": 2}
+        self.__valid_sources = ["https://www.youtube.com/watch?v=", "https://youtu.be/"]
+        self.__get_URL = {
+            0 : self.__get_YT_URL,
+            1 : self.__get_spotify_URL,
+            2 : self.__get_soundcloud_URL
+        }
+    
+    async def __join(self, ctx):
+        '''Joins voice channel if possible'''
+        channel = ctx.message.author.voice.channel
+        if channel == None:
+            await ctx.send("You must be in a voice channel to play music!")
+        else:
+            voice = get(self.bot.voice_clients, guild=ctx.guild)
+            if voice and voice.is_connected():
+                self.queue = [self.queue[-1]]
+                await voice.move_to(channel)
+            else:
+                voice = await channel.connect()
+
+    #play/add to queue
+    @commands.command(name = "play")
+    async def play(self, ctx, query):
+        '''Give URL or search term | sources: -y = YT, -s = Spotify, -c = Soundcloud (default: YT)'''
+        URL = self.get_URL(query)
+        if URL == None:
+            ctx.send("No results found :(")
+        #add song to queue
+        else:
+            self.queue.append({
+                "url" : URL,
+                "requester": ctx.author
+            })
+            ctx.send(f"Added {URL} to queue [{len(self.queue)}]")
+
+    #stop
+    @commands.command(name = "stop")
+    async def stop(self, ctx):
+        '''Stops playing music and leaves the call'''
+        pass
+
+    #private methods and utilities
+    async def __find_source(self, input):
+        try:
+            source = self.__sources[input[-2:]]
+            return input, source
+        except:
+            source = self.__sources["-y"]
+            return input + "-y", source
+    
+    async def __get_YT_URL(self, query):
+        '''searches YouTube and returns URL of first result'''
+        search = self.__youtube.search().list(q=query, type="video", part="snippet").execute()
+        try:
+            print(f"YT query: {query}")
+            video_id = search["items"][0]["id"]["videoId"]
+            URL = self.__valid_sources[0] + video_id
+            return URL
+        except:
+            return None
+
+    async def __get_spotify_URL(self, query):
+        '''searches spotify and returns URL of first result'''
+        print(f"__get_spotify_URL({query}) called; function still incomplete")
+        return None
+
+    async def __get_soundcloud_URL(self, query):
+        '''searches soundcloud and returns URL of first result'''
+        print(f"__get_soundcloud_URL({query}) called; function still incomplete")
+        return None
+
+    async def get_URL(self, input):
+        '''checks input'''
+        for s in self.__valid_sources:
+            if len(input) > len(s) and input.startswith(s):
+                return input
+        input, source = await self.__find_source(input)
+        URL = await self.__get_URL[source](input[:-2])
+        return URL
+
 
 class MusicManager():
     def __init__(self):
@@ -50,5 +142,5 @@ class MusicManager():
         input, source = await self.__find_source(input)
         URL = await self.__get_URL[source](input[:-2])
         return URL
-        
+
         
