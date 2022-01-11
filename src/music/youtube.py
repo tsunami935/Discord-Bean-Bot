5bd0d42765d3612cafce306d12e105fa435dd9f3
Nam <ndbui03@gmail.com>
2022-01-10 19:44:50 -0500

    moved from youtube data api to youtube-search-python

--- src/music/youtube.py
+++ src/music/youtube.py
@@ -1,15 +1,37 @@
-from apiclient.discovery import build
+from youtubesearchpython.__future__ import VideosSearch
+import re
 
 class Youtube_Client:
     def __init__(self):
-        self.__youtube = build('youtube','v3', developerKey = os.getenv("GOOGLE_TOKEN"))
+        pass
+    
+    async def search_video(self, query):
+        '''Searches Youtube and returns the first result'''
+        search = VideosSearch(query, limit=1)
+        video = await search.next()
+        return  {
+            'url' : video['result'][0]['link'],
+            'title' : video['result'][0]['title'],
+            'length' : video['result'][0]['duration']
+        }
 
-    def get_video(self, query):
-        '''Searches Youtube and returns video URL of first result'''
-        search = self.__youtube.search().list(q=query, type="video", part="snippet").execute()
-        try:
-            video_id = search["items"][0]["id"]["videoId"]
-            return "https://www.youtube.com/watch?v=" + video_id
-        except:
-            return None
+    #private methods
+    def __parse_title(self, title):
+        for key in self.__strs.keys():
+            if key in title:
+                title = title.replace(key, self.__strs[key])
+        return title
+    
+    def __parse_duration(self, duration):
+        return re.sub("[^0-9.]", ":", duration)
+
             
+#testing
+def main():
+    from dotenv import load_dotenv
+    load_dotenv()
+    yt = Youtube_Client()
+    print(yt.get_video("Gawr Gura Love Story"))
+
+if __name__ == "__main__":
+    main()
